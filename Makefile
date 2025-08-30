4feaf1ce73bc041f057d01d49018a7473c620251
zuzanapiarova <zuzana.piarova1@gmail.com>
2025-08-30 23:19:24 +0200

    adminer and own posts done

--- Makefile
+++ Makefile
@@ -1,26 +1,28 @@
-DOCKER_COMPOSE=docker compose
-
 DOCKER_COMPOSE_FILE = ./srcs/docker-compose.yml
 
-build:
-	mkdir -p /home/zpiarova/data/wordpress-database
+up:
+	mkdir -p /home/zpiarova/data/wordpress-data
 	mkdir -p /home/zpiarova/data/wordpress-site
-	@$(DOCKER_COMPOSE)  -f $(DOCKER_COMPOSE_FILE) up --build -d
+
+	sudo chown -R 33:33 /home/zpiarova/data/wordpress-data
+	sudo chown -R 999:999 /home/zpiarova/data/wordpress-site
+
+	docker compose  -f $(DOCKER_COMPOSE_FILE) up --build -d
 
 kill:
-	@$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) kill
+	docker compose -f $(DOCKER_COMPOSE_FILE) kill
 
 down:
-	@$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down
+	docker compose -f $(DOCKER_COMPOSE_FILE) down
 
 clean:
-	@$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down -v
+	docker compose -f $(DOCKER_COMPOSE_FILE) down -v
 
 fclean: clean
-	rm -rf /home/zpiarova/data/wordpress-database
+	rm -rf /home/zpiarova/data/wordpress-data
 	rm -rf /home/zpiarova/data/wordpress-site
 	docker system prune -a -f
 
 restart: clean build
 
-.PHONY: kill build down clean restart
+.PHONY: kill up down clean restart
--- srcs/requirements/wordpress/tools/wp-setup.sh
+++ srcs/requirements/wordpress/tools/wp-setup.sh
@@ -1,48 +1,45 @@
-#!bin/bash
+#! /bin/bash
 
-sleep 10
+set -e 
 
-# setting up WordPress via WP-CLI automatically instead of manually in the browser
-
-# only run  setup if WordPress hasn’t been configured yet - checks if setup file wp-config.php exists in /var/www/html
+# # only run  setup if WordPress hasn’t been configured yet - checks if setup file wp-config.php exists in /var/www/html
 if [ ! -f /var/www/html/wp-config.php ]; then
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER \
-        --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root  --skip-check
-    
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root  --skip-check
+fi
+
+until wp db check --allow-root; do
+    echo "Waiting for database to be ready..."
+    sleep 3
+done
+
+if ! wp core is-installed --allow-root; then
     # installs WordPress core: sets site URL(--url), ets site title (--title), creates admin account (--admin_user, --admin_password, --admin_email)
-    wp core install --url=$DOMAIN_NAME --title="INCEPTION" --admin_user=$WORDPRESS_ADMIN_USER \
-        --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL \
-        --allow-root
+    wp core install --url=$DOMAIN_NAME --title="INCEPTION" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
     
     # create additional WordPress user
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 
- #   wp config  set WP_DEBUG true  --allow-root
+    HOME_ID=$(wp post create \
+        --post_type=page \
+        --post_title="Home" \
+        --post_content="<h1 style='color:#FFB6C1;'>Welcome to Inception project by zpiarova</h1>" \
+        --post_status=publish \
+        --porcelain --allow-root) || true
 
-    # wp config set FORCE_SSL_ADMIN 'false' --allow-root
+    wp option update show_on_front 'page' --allow-root || true
+    wp option update page_on_front $HOME_ID --allow-root || true
 
-    # wp config  set WP_REDIS_HOST $redis_host --allow-root
+#     # ---------------------------
+#     # Create 3 blog posts
+#     # ---------------------------
+    wp post create --post_title="First Post" --post_content="<p style='color:#FFB6C1;'> This is the first blog post.</p>" --post_status=publish --post_author=1 --allow-root || true
+    wp post create --post_title="Second Post" --post_content="<p style='color:#FFB6C1;'>This is the second blog post.</p>" --post_status=publish --post_author=1 --allow-root || true
+    wp post create --post_title="Third Post" --post_content="<p style='color:#FFB6C1;'>This is the third blog post.</p>" --post_status=publish --post_author=1 --allow-root || true
 
-    # wp config set WP_REDIS_PORT $redis_port --allow-root
+    wp option update show_on_front 'posts' --allow-root || true
 
-    # wp config  set WP_CACHE 'true' --allow-root
+    echo "landing page setup complete"
 
-    # wp plugin install redis-cache --allow-root
-
-    # wp plugin activate redis-cache --allow-root
-
-    # wp redis enable --allow-root
-
-    chown -R www-data:www-data /var/www/html/wp-content
-    chmod -R 755 /var/www/html/wp-content
-
-    # install theme
-
-    wp theme install twentyfifteen
-
-    wp theme activate twentyfifteen
-
-    wp theme update twentyfifteen
 fi
 
 /usr/sbin/php-fpm7.4 -F
